b649fa77aca1d0d5a6db0e1eddad5275e9540938
tgiarrat <tgiarrat@calpoly.edu>
2017-04-12 12:43:07 -0700

    setting up ethernet struct

--- ethernet.h
+++ ethernet.h
@@ -1,4 +1,11 @@
 #include <stdint.h>
 
 
+typedef struct ehter_struct{
+    char dest[MAC_LEN]; //destination mac addr
+    char src[MAC_LEN]; // source mac adddr
+    uint16_t type; //tpye of protocol
+}__attribute__((packed)); 
+
+
 int print_ether_info(uint8_t *);
